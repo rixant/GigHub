89d154b6bc6f60e7ce36ff324c7a80a259adff07
Rishant Rokaha <Rrokaha@Langan.com>
2021-06-15 11:30:48 -0400

    Gig and Genre Table created

--- GigHub/Models/Genre.cs
+++ GigHub/Models/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,11 @@ namespace GigHub.Models
 {
     public class Genre
     {
+        public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
     }
+
 }
--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,19 @@ namespace GigHub.Models
     public class Gig
     {
         public int Id { get; set; }
+
+        [Required]
         public ApplicationUser Artist { get; set; }
+        
 
         public DateTime DateTime { get; set; }
 
+        [Required]
+        [StringLength(255)]
+
         public string Venue { get; set; }
 
+        [Required]
         public Genre Genre { get; set; }
     }
 
